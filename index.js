e1da165740f76d70ad0c8cfacb6ec9e3b1f595df
shathi <shathi@gmail.com>
2023-11-28 03:36:14 +0600

    create api for user, filter data and create api for cancel request and also create api for return

--- index.js
+++ index.js
@@ -93,7 +93,6 @@ async function run() {
 
         app.get('/all-asset', async (req, res) => {
             const queryValue = req.query
-            console.log(queryValue.type)
 
             if (queryValue.search) {
                 const query = {
@@ -106,7 +105,6 @@ async function run() {
                 const query = { assetQuantity: { $gt: 0 } };
                 const result = await AssetCollection.find(query).toArray()
                 res.send(result)
-                console.log('Hello Bangladesh')
             }
 
 
@@ -139,7 +137,6 @@ async function run() {
         app.post('/send-request', async (req, res) => {
             const requestData = req.body
             const assetId = requestData.singleAsset._id
-            console.log(assetId)
             const filter = { _id: new ObjectId(assetId) }
             const UpdateAsset = {
                 $inc: {
@@ -147,7 +144,6 @@ async function run() {
                 }
             }
             const updateAssets = await AssetCollection.updateOne(filter, UpdateAsset)
-            console.log(updateAssets)
             const result = await RequestCollection.insertOne(requestData)
             res.send({ result, updateAssets })
         })
@@ -231,6 +227,106 @@ async function run() {
             const result = await TeamtCollection.findOne(query)
             res.json(result)
         })
+
+        app.get('/my-asset/:email', async (req, res) => {
+            const Email = req.params.email
+            const queryValue = req.query
+
+            if (queryValue.fiter == 'pending') {
+                const combinedFilter = {
+                    $and: [
+                        { userEmail: Email },
+                        // {assetType: queryValue.type},
+                        { isAprove: false }
+                    ]
+                };
+                const result = await RequestCollection.find(combinedFilter).toArray()
+                res.send(result)
+            }
+            else if (queryValue.fiter == 'aprove') {
+                const combinedFilter = {
+                    $and: [
+                        { userEmail: Email },
+                        // {assetType: queryValue.type},
+                        { isAprove: true }
+                    ]
+                };
+                const result = await RequestCollection.find(combinedFilter).toArray()
+                res.send(result)
+            }
+
+            else if (queryValue.fiter == 'returnable') {
+                const combinedFilter = {
+                    $and: [
+                        { userEmail: Email },
+                        // {assetType: queryValue.type},
+                        { 'singleAsset.assetType': 'returnable' }
+                    ]
+                };
+                const result = await RequestCollection.find(combinedFilter).toArray()
+                res.json(result)
+            }
+            else if (queryValue.fiter == 'nonreturnable') {
+                const combinedFilter = {
+                    $and: [
+                        { userEmail: Email },
+                        // {assetType: queryValue.type},
+                        { 'singleAsset.assetType': 'nonreturnable' }
+                    ]
+                };
+                const result = await RequestCollection.find(combinedFilter).toArray()
+                res.json(result)
+            }
+            else if (queryValue.search) {
+                const combinedFilter = {
+                    $and: [
+                        { userEmail: Email },
+                        // {assetType: queryValue.type},
+                        { 'singleAsset.assetName': { $regex: queryValue.search, $options: 'i' } }
+                    ]
+                };
+                const result = await RequestCollection.find(combinedFilter).toArray()
+                res.json(result)
+            }
+            else {
+                const query = { userEmail: Email }
+                const result = await RequestCollection.find(query).toArray()
+                res.send(result)
+            }
+
+        })
+
+        app.delete('/cansel-requst/:id', async (req, res) => {
+            const Id = req.params.id
+            const query = { _id: new ObjectId(Id) }
+            const result = await RequestCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.put('/update-request/:id', async (req, res) => {
+            const Id = req.params.id
+            const AssetId = req.query
+            const assetId = AssetId.assetId
+            const filter = { _id: new ObjectId(assetId) }
+            const UpdateAsset = {
+                $inc: {
+                    assetQuantity: +1
+                }
+            }
+            const query = {_id: new ObjectId(Id)}
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    retured: true
+                },
+            };
+            const updateAssets = await AssetCollection.updateOne(filter, UpdateAsset)
+            const result = await RequestCollection.updateOne(query, updateDoc, options)
+
+            res.send({result, updateAssets})
+
+            })
+
 
 
         // Send a ping to confirm a successful connection
