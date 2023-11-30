5d4f8ff53aab11e0613ac9c2a135272ed7fdc4c8
rakib <hassanrakibul926@gmail.com>
2023-11-30 19:12:35 +0600

    deployment issue solv

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
 
 
         const userCollection = client.db('assetManagement').collection('user')
@@ -319,6 +318,8 @@ async function run() {
 
         app.get('/admin-request/:email', async (req, res) => {
             const Email = req.params.email
+            console.log(Email)
+            console.log('--------------------------')
             const filter = { 'singleAsset.email': Email }
             const result = await RequestCollection.find(filter).toArray()
             res.json(result)
@@ -602,7 +603,6 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
